62ed68f9a60178fdf16a163ebc542aea826ce94c
tomas-ribeiro-pinto <morato.toms@outlook.com>
2023-04-19 22:17:07 +0100

    Style Changes

--- templates/login.php
+++ templates/login.php
@@ -1,12 +1,22 @@
-
 <?php echo $message; ?>
 
-<form name="frmLogin" action="authenticate.php" method="post">
-   Student ID:
-   <input name="txtid" type="text" />
-   <br/>
-   Password:
-   <input name="txtpwd" type="password" />
-   <br/>
-   <input type="submit" value="Login" name="btnlogin" />
-</form>
+<div class="row h-100 d-flex align-items-center justify-content-center">
+   <div>
+      <div class="col-3 p-3 m-auto text-center bg-dark">
+         <h1 class="text-white">Login to Student Centre</h1>
+      </div>
+      <div class="col-3 p-3 m-auto bg-warning">
+      <form name="frmLogin" action="authenticate.php" method="post">
+         Student ID:
+         <input class="form-control" name="txtid" type="text" />
+         <br/>
+         Password:
+         <input class="form-control" name="txtpwd" type="password" />
+         <br/>
+         <div class="">
+            <button class="btn btn-dark w-100" type="submit" name="btnlogin">Login</button>
+         </div>
+      </form>
+   </div>
+   </div>
+</div>
--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -1,9 +1,9 @@
 
-<div id="main-nav">
+<div class="p-4" id="main-nav">
 
-   <a href="modules.php">My Modules</a> |
-   <a href="assignmodule.php">Assign Module</a> |
-   <a href="details.php">My Details</a> |
-   <a href="logout.php">Logout</a>
+   <a class="btn btn-primary" href="modules.php">My Modules</a>
+   <a class="btn btn-dark" href="assignmodule.php">Assign Module</a>
+   <a class="btn btn-dark" href="details.php">My Details</a>
+   <a class="btn btn-danger" href="logout.php">Logout</a>
 
 </div>
